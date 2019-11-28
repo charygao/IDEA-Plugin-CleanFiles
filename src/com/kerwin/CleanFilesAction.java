08ee82333863ddcc93236b550c3c8394e0381e81
柯贤铭 <kexianming@testin.cn>
2019-11-28 20:19:12 +0800

    pushs

---
+++ src/com/kerwin/CleanFilesAction.java
@@ -0,0 +1,16 @@
+package com.kerwin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Kerwin on 2019/11/28.
+ */
+public class CleanFilesAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+    }
+}
